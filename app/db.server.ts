3570cb578ee8a002e5e0fce3b1a16ca2a1524135
hupptechnologies <git@hupp.in>
2025-03-11 13:56:30 +0530

    Fix ESLint issues (function parens, duplicate imports, double to single quotes)

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  var prisma: PrismaClient;
+	const prisma: PrismaClient;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+if (process.env.NODE_ENV !== 'production') {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
 }
 
 const prisma: PrismaClient = global.prisma || new PrismaClient();
