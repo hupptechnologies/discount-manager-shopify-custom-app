993ac19362ea415b3ed3875fdb75b4e38d664325
hupptechnologies <git@hupp.in>
2025-03-19 10:52:19 +0530

    Create DiscountCode model, migration and add postman collection

---
+++ prisma/migrations/dicount_code_table/migration.sql
@@ -0,0 +1,19 @@
+CREATE TYPE "DiscountType" AS ENUM ('FIXED', 'PERCENT');
+
+CREATE TABLE "DiscountCode" (
+  "id" SERIAL PRIMARY KEY,
+  "code" VARCHAR(255) NOT NULL UNIQUE,
+  "title" VARCHAR(255) NOT NULL,
+  "description" TEXT,
+  "discountAmount" FLOAT NOT NULL,
+  "discountType" "DiscountType" NOT NULL,
+  "isActive" BOOLEAN DEFAULT true,
+  "startDate" TIMESTAMPTZ NOT NULL,
+  "endDate" TIMESTAMPTZ NOT NULL,
+  "usageLimit" INT NOT NULL,
+  "usedCount" INT DEFAULT 0,
+  "createdAt" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX "DiscountCode_code_index" ON "DiscountCode"("code");
