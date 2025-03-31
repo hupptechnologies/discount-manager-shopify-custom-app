8fcf87a369f5c325de00b4c82c7a027ac6b97dc3
hupptechnologies <git@hupp.in>
2025-03-31 15:47:20 +0530

    Add logic to insert discount entries in DB when created via admin

--- app/routes/webhooks.app.discounts.create.tsx
+++ app/routes/webhooks.app.discounts.create.tsx
@@ -1,8 +1,19 @@
-import type { ActionFunctionArgs } from '@remix-run/node';
 import { authenticate } from '../shopify.server';
+import { handleDiscountCreate } from 'app/webhookHandler/handleDiscountCreate';
+import type { ActionFunctionArgs } from '@remix-run/node';
 
-export const action = async ({ request }: ActionFunctionArgs) => {
+interface WebhookPayload {
+	admin_graphql_api_id: string;
+	title: string;
+	status: string;
+	created_at: string;
+	updated_at: string;
+}
+
+export const action = async ({ request }: ActionFunctionArgs): Promise<Response> => {
 	const { shop, topic, payload } = await authenticate.webhook(request);
-	console.log(`Received ${topic} webhook for ${shop}`, payload);
+	const typedPayload = payload as WebhookPayload;
+	console.log(`Received ${topic} webhook for ${shop}`);
+	handleDiscountCreate(typedPayload, shop);
 	return new Response();
 };
---
+++ app/webhookHandler/handleDiscountCreate.tsx
@@ -0,0 +1,121 @@
+import prisma from '../db.server';
+import { getDetailUsingGraphQL } from 'app/service/product';
+
+const GET_BASIC_DISCOUNT_CODE_QUERY = `
+query getDiscountCode($ID: ID!) {
+	codeDiscountNode(id: $ID) {
+		codeDiscount {
+			... on DiscountCodeBasic {
+				status
+				title
+				startsAt
+				endsAt
+				discountClass
+				codes(first: 1) {
+					edges {
+						node {
+							code
+						}
+					}
+				}
+				customerGets {
+					value {
+						...on DiscountPercentage {
+							percentage
+						}
+					}
+				}
+				usageLimit
+				appliesOncePerCustomer
+			}
+		}
+	}
+}`;
+
+interface PayloadDiscountCreate {
+	admin_graphql_api_id: string;
+	title: string;
+	status: string;
+	created_at: string;
+	updated_at: string;
+}
+
+interface GraphQLResponse {
+	data: {
+		data: {
+			codeDiscountNode: {
+				codeDiscount: {
+					discountClass: string;
+					usageLimit: number;
+					title: string;
+					codes: {
+						edges: Array<{
+							node: {
+								code: string;
+							};
+						}>;
+					};
+					startsAt: string;
+					endsAt: string;
+					customerGets: {
+						value: {
+							percentage: number;
+						};
+					};
+				};
+			};
+		};
+	};
+}
+
+export const handleDiscountCreate = async (
+	payload: PayloadDiscountCreate,
+	shop: string
+): Promise<void> => {
+	try {
+		const response = await prisma.session.findMany({
+			where: { shop },
+		});
+		const accessToken = response[0]?.accessToken;
+
+		if (!accessToken) {
+			throw new Error('Access token not found');
+		}
+		const data = {
+			query: GET_BASIC_DISCOUNT_CODE_QUERY,
+			variables: {
+				ID: payload.admin_graphql_api_id,
+			},
+		};
+		const graphQlResponse: GraphQLResponse = await getDetailUsingGraphQL(shop, accessToken, data);
+
+		const {
+			discountClass,
+			usageLimit,
+			title,
+			codes: { edges },
+			startsAt,
+			endsAt,
+			customerGets: { value },
+		} = graphQlResponse?.data?.data?.codeDiscountNode?.codeDiscount;
+
+		await prisma.discountCode.create({
+			data: {
+				code: edges[0]?.node?.code,
+				title,
+				shop,
+				discountId: payload.admin_graphql_api_id,
+				startDate: new Date(startsAt),
+				endDate: new Date(endsAt),
+				discountAmount: Number(value?.percentage * 100),
+				discountType: 'PERCENT',
+				usageLimit,
+				isActive: true,
+				discountMethod: 'CUSTOM',
+				discountScope: discountClass === 'PRODUCT' ? 'PRODUCT' : discountClass === 'ORDER' ? 'ORDER' : discountClass === 'SHIPPING' ? 'SHIPPING' : 'BUYXGETY' ,
+			},
+		});
+	} catch (error) {
+		console.error('Error in discount create webhook handler', error);
+	}
+};
