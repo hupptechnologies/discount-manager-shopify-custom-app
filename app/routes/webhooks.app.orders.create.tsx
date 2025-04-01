5d8992540a612684fac1a968e2ca015784f53e8b
hupptechnologies <git@hupp.in>
2025-04-01 15:59:31 +0530

    Add order creation webhook to increment used count and update file name

--- app/routes/webhooks.app.orders.create.tsx
+++ app/routes/webhooks.app.orders.create.tsx
@@ -1,8 +1,12 @@
 import type { ActionFunctionArgs } from '@remix-run/node';
 import { authenticate } from '../shopify.server';
+import { handleOrderCreate } from 'app/webhookHandler/handleOrderCreate';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	const { topic, shop } = await authenticate.webhook(request);
+	const { topic, shop, payload } = await authenticate.webhook(request);
 	console.log(`Received ${topic} webhook for ${shop}`);
+	if (payload?.discount_codes?.length > 0 && payload?.customer?.id) {
+		handleOrderCreate(shop, payload?.discount_codes);
+	}
 	return new Response();
 };
---
+++ app/webhookHandler/handleOrderCreate.ts
@@ -0,0 +1,26 @@
+import prisma from "app/db.server";
+
+export const handleOrderCreate = async (shop: string, discount_codes: any[]) => {
+	try {
+		for (const application of discount_codes) {
+			const discountCode = application.code;
+			const discount = await prisma.discountCode.findFirst({
+				where: { shop, code: discountCode },
+			});
+			if (discount) {
+				await prisma.discountCode.update({
+					where: {
+						id: discount.id,
+					},
+					data: {
+						usedCount: {
+							increment: 1
+						}
+					},
+				});
+			}
+		}
+	} catch (error) {
+		console.error('Error in order create webhook handler', error);
+	}
+};
