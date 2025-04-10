1cb0e1e2609bdb9d573336166f81ef1251425378
hupptechnologies <git@hupp.in>
2025-04-10 14:05:55 +0530

    Create API to get all product categories and update collection

---
+++ app/controller/products/fetchProductsCategories.ts
@@ -0,0 +1,59 @@
+import prisma from "app/db.server";
+import { FetchCategoryResponse } from "app/routes/api.products/route";
+import { getDetailUsingGraphQL } from "app/service/product";
+
+const  GET_CATEGORIES_QUERY = (childrenOf: string) => `
+query GetAllCategories {
+	taxonomy {
+		categories(first: 250${childrenOf ? `, childrenOf: "${childrenOf}"` : ''}) {
+			edges {
+				node {
+					id
+					name
+					fullName
+					childrenIds
+				}
+			}
+		}
+	}
+}`;
+
+interface GraphqlResponse {
+	data: {
+		data: {
+			taxonomy:{
+				categories: {
+					edges: {
+						node: {
+							id: string;
+							name: string;
+							fullName: string;
+							childrenIds: string;
+						};
+					}[];
+				};
+			};
+		};
+	};
+};
+
+export const fetchAllCategories = async (shop: string, childrenOf: string): Promise<FetchCategoryResponse> => {
+	try {
+		const response = await prisma.session.findMany({
+			where: { shop },
+		});
+		const accessToken = response[0]?.accessToken;
+		if (!accessToken) {
+			throw new Error('Access token not found');
+		}
+		const data = {
+			query: GET_CATEGORIES_QUERY(childrenOf)
+		};
+		const taxonomyRes: GraphqlResponse = await getDetailUsingGraphQL(shop, accessToken, data);
+		const categories = taxonomyRes?.data?.data?.taxonomy?.categories?.edges || [];
+		return { success: true, message: 'Retrive all products categories', categories };
+	} catch (error) {
+		console.log(error, 'Error while fetching categories');
+		return { success: false, message: 'Something went wrong', categories: [] };
+	}
+};
--- app/routes/api.products/route.tsx
+++ app/routes/api.products/route.tsx
@@ -2,6 +2,7 @@ import { json } from '@remix-run/node';
 import { fetchProducts } from '../../controller/products/fetchProducts';
 import type { FetchProductVariantsResult } from 'app/controller/products/fetchProductVariants';
 import { fetchProductVariants } from 'app/controller/products/fetchProductVariants';
+import { fetchAllCategories } from 'app/controller/products/fetchProductsCategories';
 
 export interface ProductVariant {
 	id: string;
@@ -24,26 +25,34 @@ interface LoaderResponse {
 	totalCount: number;
 }
 
+export interface FetchCategoryResponse {
+	success: boolean;
+	message: string;
+	categories: object | null;
+}
+
 export const loader = async ({
 	request,
 }: {
 	request: Request;
 }): Promise<Response> => {
 	const url = new URL(request.url);
-
 	const shop = url.searchParams.get('shop') ?? '';
 	const after = url.searchParams.get('after') ?? null;
 	const before = url.searchParams.get('before') ?? null;
 	const query = url.searchParams.get('query') ?? '';
-
 	const id = url.searchParams.get('id');
-
+	const type = url.searchParams.get('type');
+	const childrenOf = url.searchParams.get('childrenOf') ?? '';
+	if (type === 'category') {
+		const response = await fetchAllCategories(shop, childrenOf);
+		return json<FetchCategoryResponse>(response);
+	}
 	if (id) {
 		const response = await fetchProductVariants(shop, id);
 		return json<FetchProductVariantsResult>(response);
 	} else {
 		const response = await fetchProducts(shop, after, before, query);
-
 		return json<LoaderResponse>({
 			products: response.products,
 			pageInfo: response.pageInfo,
